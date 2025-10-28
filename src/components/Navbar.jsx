03260955d607a0ef411d793b1404988c952bd2e2
ishordev1 <chyraj63@gmail.com>
2025-10-28 22:06:37 +0530

    navbar created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,66 @@
+import { Button } from "@/components/ui/button"
+import Link from 'next/link';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { ModeToggle } from "./Theme-Btn";
+const Navbar = () => {
+  return (
+    <>
+      <nav className=" p-2 bg-background/50 sticky top-0 backdrop-blur border-b z-10 ">
+        <div className="container mx-auto flex items-center justify-between">
+          <div className="font-bold text-lg">
+            MyBlog
+          </div>
+          <div className="md:flex space-x-4 hidden">
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/contact">Contact</Link>
+            <Link href="/blog">Blog</Link>
+          </div>
+          <div className="md:flex hidden items-center ">
+            <ModeToggle />
+            <Button variant="outline" className="mx-1">Login</Button>
+            <Button variant="outline" className="mx-1">Signup</Button>
+          </div>
+          <div className="md:hidden flex items-center" variant="outline">
+ <ModeToggle />
+            <Sheet>
+              <SheetTrigger className=" p-2 rounded"><svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+              </svg></SheetTrigger>
+              <SheetContent side="left">
+                <SheetHeader>
+                  <SheetTitle className="font-bold mt-4">Construction</SheetTitle>
+                  <SheetDescription>
+                    <div className="flex flex-col gap-1">
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/contact">Contact</Link>
+            <Link href="/blog">Blog</Link>
+          
+          <div className="flex items-center mt-3">
+            <Button variant="outline" className="text-xs">Login</Button>
+            <Button variant="outline" className="mx-1 text-xs">Signup</Button>
+          </div>
+          </div>
+                  </SheetDescription>
+                </SheetHeader>
+              </SheetContent>
+            </Sheet>
+          </div>
+
+
+        </div>
+      </nav>
+
+
+    </>
+  );
+};
+export default Navbar;
